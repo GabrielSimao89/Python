99ef21021000f857500a13e2e532de04c331e9ba
Gabriel Simão <gabriel.simao.t0129860@edu.atec.pt>
2025-10-03 15:28:34 +0100

    add exercicio 1

---
+++ Exercício1.py
@@ -0,0 +1,5 @@
+segundos = int(input("Digite os segundos: "))
+horas = segundos // 3600
+minutos = (segundos % 3600) // 60
+segundos_restantes = segundos % 60
+print(horas, "hora(s),", minutos, "minuto(s) e", segundos_restantes, "segundo(s)")
