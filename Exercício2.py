fb83ddf1b6d3d0bc485998cb4ca2cd807792c230
Gabriel Simão <gabriel.simao.t0129860@edu.atec.pt>
2025-10-15 14:11:34 +0100

    Resolução do Exercício 2 em Python

---
+++ Exercício2.py
@@ -0,0 +1,12 @@
+#Gabriel Simão        15/10/2025               PI0924
+
+
+print("Digite 3 números:")
+num1 = int(input("Primeiro: "))
+num2 = int(input("Segundo: "))
+num3 = int(input("Terceiro: "))
+
+maior = max(num1, num2, num3)
+menor = min(num1, num2, num3)
+
+print("Maior", maior, "menor", menor)
