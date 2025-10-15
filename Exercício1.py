f5ca432806becfdda3eaf08996b5479ad4253675
Gabriel Simão <gabriel.simao.t0129860@edu.atec.pt>
2025-10-15 14:38:40 +0100

    Resolução do Exercício2+

--- Exercício1.py
+++ Exercício1.py
@@ -2,5 +2,5 @@ segundos = int(input("Digite os segundos: "))
 horas = segundos // 3600
 minutos = (segundos % 3600) // 60
 segundosrestantes = segundos % 60
-print(horas, "horas,", minutos, "minuto(s) e", segundosrestantes, "segundo(s)")
+print(horas, "horas,", minutos, "minuto(s) e", segundosrestantes, "segundo(s)") 
  
--- Exercício2.py
+++ Exercício2.py
@@ -2,11 +2,25 @@
 
 
 print("Escolhe 3 números:")
+
 num1 = int(input("Primeiro: "))
 num2 = int(input("Segundo: "))
 num3 = int(input("Terceiro: "))
 
-maior = max(num1, num2, num3)
-menor = min(num1, num2, num3)
+# Encontrar o MAIOR usando ifs
+if num1 >= num2 and num1 >= num3:
+    maior = num1
+elif num2 >= num1 and num2 >= num3:
+    maior = num2
+else:
+    maior = num3
 
-print("maior", maior, "menor", menor) 
+# Encontrar o MENOR usando ifs
+if num1 <= num2 and num1 <= num3:
+    menor = num1
+elif num2 <= num1 and num2 <= num3:
+    menor = num2
+else:
+    menor = num3
+
+print("maior", maior, "menor", menor)
