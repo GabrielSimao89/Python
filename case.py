a455a1ee60a8c09239124608aad5eb27e39e165d
Gabriel Simão <gabriel.simao.t0129860@edu.atec.pt>
2025-10-21 14:47:31 +0100

    Pedra papel e tesoura

---
+++ case.py
@@ -0,0 +1,21 @@
+opc=0
+
+while True:
+    print("1- Para receber um bom dia!")
+    print("2- Para receber uma boa tarde!")
+    print("3- Para receber uma boa noite!")
+    print("4-Sair!")
+
+    opc=int(input("Escolhe uma opção!"))
+
+    match opc: 
+        case 1:
+            print("Bom Dia!")
+        case 2:
+            print("Boa Tarde!")
+        case 3:
+            print("Boa Noite!")
+        case 4:
+            print("Xau Xau!")
+            break 
+
